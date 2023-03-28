54d82e4fd6fb1f53bf21d0d9fdffd451913bc5ab
KarthikRaja0517 <116252119+KarthikRaja0517@users.noreply.github.com>
2023-03-28 11:16:47 +0530

    feat: User CRUD

---
+++ pages/user_interface_display.js
@@ -0,0 +1,5 @@
+// const user_email =localstorage.getitem(user_email)
+// console log(user_email)
+create id for the respective data showing element inorder to 
+// document.getelementbyid (id-on-the-respective-tag).innerhtml=user-email
+// input field document.getelementbyid(id-on-the-respective-tag).value=user-email
